e2128d7c2135244c46ffa42c671b63c83d46a353
Shahar Vardi <n0troot@ownd.lol>
2024-12-05 11:10:17 +0200

    Better output

--- HashcatAppendCracked.ps1
+++ HashcatAppendCracked.ps1
@@ -1,29 +1,38 @@
 function cut {
-  param(
-    [Parameter(ValueFromPipeline=$True)] [string]$inputobject,
-    [string]$delimiter='\s+',
-    [string[]]$field
-  )
- 
-  process {
-    if ($field -eq $null) { $inputobject -split $delimiter } else {
-      ($inputobject -split $delimiter)[$field] }
-  }
+    param(
+        [Parameter(ValueFromPipeline=$True)] [string]$inputobject,
+        [string]$delimiter='\s+',
+        [string[]]$field
+    )
+    process {
+        if ($field -eq $null) { $inputobject -split $delimiter } else {
+            ($inputobject -split $delimiter)[$field] }
+    }
 }
+
+$outputArray = @()
+
 foreach ($line in $list) {
-     $ln = (cut $line -delimiter ':')[-4]
-     foreach ($rs in $results) {
-         if ($rs.StartsWith($ln)) {
-             $rs1 = ($rs -split ":")[1]
-             if ($line -match "(status=Enabled)") {
-                 $line = $line.Replace("(status=Enabled)","");
-                 Write-Output "$line$rs1"
-             } elseif ($line -match "(status=Disabled)") {
-                 $line = $line.Replace("(status=Disabled)","");
-                 Write-Output "$line$rs1"
-             } else {
-                 Write-Output "$line$rs1"
-             }
-          }
-     }
- }
+    $ln = (cut $line -delimiter ':')[-4]
+    $foundMatch = $false
+    
+    # Remove status if present
+    $cleanLine = $line -replace "\(status=(?:Enabled|Disabled)\)",""
+    
+    foreach ($rs in $results) {
+        if ($rs.StartsWith($ln)) {
+            $foundMatch = $true
+            $rs1 = ($rs -split ":")[1]
+            $outputArray += "$cleanLine$rs1"
+            break  # Exit the loop once we find a match
+        }
+    }
+    
+    # If no match was found, output the original line (cleaned)
+    if (-not $foundMatch) {
+        $outputArray += $cleanLine
+    }
+}
+
+# Output to file
+$outputArray | Out-File -FilePath "output.txt" -Encoding UTF8
